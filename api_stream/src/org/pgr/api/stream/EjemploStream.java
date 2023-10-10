2f92c4b0c996f11e7baa25f2f4c02600c2ffe94b
rperez1002 <rperez.tae@gmail.com>
2023-10-10 14:50:30 -0600

    them.

---
+++ api_stream/src/org/pgr/api/stream/EjemploStream.java
@@ -0,0 +1,11 @@
+package org.pgr.api.stream;
+
+import java.util.stream.Stream;
+
+public class EjemploStream {
+    public static void main(String[] args) {
+
+        Stream<String> nombres = String.of("Lucas", "Paco", "Pepe", "Pig");
+
+    }
+}
